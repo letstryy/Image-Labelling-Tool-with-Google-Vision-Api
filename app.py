79cb206ab74ff05a98ae7ccb4aedfba7796694d1
letstryy <sneharawatt@gmail.com>
2019-05-29 21:41:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+#Dependencies
+import sys
+from os import walk
+import imghdr
+import csv
+import webbrowser
+import doctext
+import argparse
+import json
+from flask import jsonify
+from flask import Flask, redirect, url_for, request
+from flask import render_template
+from flask import send_file
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="Googleapi.json"
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+# Route for handling the login page logic
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('labeller'))
+    return render_template('login.html', error=error)
+
+@app.route('/labeller')
+def labeller():
+    if (app.config["HEAD"] == len(app.config["FILES"])):
+        return redirect(url_for('bye'))
+    directory = app.config['IMAGES']
+    image = app.config["FILES"][app.config["HEAD"]]
+    labels = app.config["LABELS"]
+    not_end = not(app.config["HEAD"] == len(app.config["FILES"]) - 1)
+    first = app.config["HEAD"]
+    bounds = doctext.render_doc_text(app.config["IMAGES"],app.config["FILES"][app.config["HEAD"]])
+    return render_template('labeller.html', not_end=not_end, first=first, directory=directory, image=image, bounds=bounds, labels=labels, head=app.config["HEAD"] + 1, len=len(app.config["FILES"]))
+
+@app.route('/next')
+def next():
+    image = app.config["FILES"][app.config["HEAD"]]
+    app.config["HEAD"] = app.config["HEAD"] + 1
+    with open(app.config["OUT"],'a') as f:
+        for label in app.config["LABELS"]:
+            f.write(image + "," +
+            label["id"] + "," +
+            label["name"] + "," +
+            str(round(float(label["xMin"]))) + "," +
+            str(round(float(label["xMax"]))) + "," +
+            str(round(float(label["yMin"]))) + "," +
+            str(round(float(label["yMax"]))) + "\n")
+    app.config["LABELS"] = []
+    return redirect(url_for('labeller'))
+
+@app.route('/discard')
+def discard():
+    image = app.config["FILES"][app.config["HEAD"]]
+    a = 'discard'
+    app.config["HEAD"] = app.config["HEAD"] + 1
+    with open(app.config["OUT"],'a') as f:
+            f.write(image + "," +
+             a + "," +
+             a + "," +
+             a + "," +
+             a + "," +
+             a + "," +
+             a + "\n")
+    app.config["LABELS"] = []
+    return redirect(url_for('labeller'))
+
+@app.route("/bye")
+def bye():
+    return send_file("taf.gif", mimetype='image/gif')
+
+@app.route('/add/<id>')
+def add(id):
+    xMin = request.args.get("xMin")
+    xMax = request.args.get("xMax")
+    yMin = request.args.get("yMin")
+    yMax = request.args.get("yMax")
+    app.config["LABELS"].append({"id":id, "name":"", "xMin":xMin, "xMax":xMax, "yMin":yMin, "yMax":yMax})
+    return redirect(url_for('labeller'))
+
+@app.route('/remove/<id>')
+def remove(id):
+    index = int(id) - 1
+    del app.config["LABELS"][index]
+    for label in app.config["LABELS"][index:]:
+        label["id"] = str(int(label["id"]) - 1)
+    return redirect(url_for('labeller'))
+
+@app.route('/label/<id>')
+def label(id):
+    name = request.args.get("name")
+    app.config["LABELS"][int(id) - 1]["name"] = name
+    return redirect(url_for('labeller'))
+
+@app.route('/prev')
+def prev():
+    image = app.config["FILES"][app.config["HEAD"]]
+    app.config["HEAD"] = app.config["HEAD"] - 1
+    return redirect(url_for('labeller'))
+
+@app.route('/image/<f>')
+def images(f):
+    images = app.config['IMAGES']
+    return send_file(images + f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dir', type=str, help='specify the images directory')
+    parser.add_argument("--out")
+    args = parser.parse_args()
+    directory = args.dir
+    if directory[len(directory) - 1] != "/":
+         directory += "/"
+    app.config["IMAGES"] = directory
+    app.config["LABELS"] = []
+    files = None
+    for (dirpath, dirnames, filenames) in walk(app.config["IMAGES"]):
+        files = filenames
+        break
+    if files == None:
+        print("No files")
+        exit()
+    app.config["FILES"] = files
+    app.config["HEAD"] = 0
+    if args.out == None:
+        app.config["OUT"] = "out.csv"
+    else:
+        app.config["OUT"] = args.out
+    print(files)
+    with open("out.csv",'w') as f:
+        f.write("image,id,name,xMin,xMax,yMin,yMax\n")
+    app.jinja_env.cache = {}
+    url = 'http://127.0.0.1:5000/'
+    webbrowser.open_new(url)
+    app.run(debug=True, threaded=True, use_reloader=False)
---
+++ doctext.py
@@ -0,0 +1,68 @@
+import argparse
+from enum import Enum
+import io
+import json
+import os, os.path
+from google.cloud import vision
+from google.cloud.vision import types
+from PIL import Image, ImageDraw
+
+class FeatureType(Enum):
+    PAGE = 1
+    BLOCK = 2
+    PARA = 3
+    WORD = 4
+    SYMBOL = 5
+
+def get_document_bounds(image_file, feature):
+
+    client = vision.ImageAnnotatorClient()
+
+    bounds = []
+
+    with io.open(image_file, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+    document = response.full_text_annotation
+
+    for page in document.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    for symbol in word.symbols:
+                        if (feature == FeatureType.SYMBOL):
+                            bounds.append(symbol.bounding_box)
+
+                    if (feature == FeatureType.WORD):
+                        bounds.append(word.bounding_box)
+
+                if (feature == FeatureType.PARA):
+                    bounds.append(paragraph.bounding_box)
+
+            if (feature == FeatureType.BLOCK):
+                bounds.append(block.bounding_box)
+
+        if (feature == FeatureType.PAGE):
+            bounds.append(block.bounding_box)
+
+    return bounds
+
+
+def render_doc_text(opn,filein):
+    image = Image.open(os.path.join(opn,filein))
+    bounds = get_document_bounds(os.path.join(opn,filein), FeatureType.PARA)
+    result = {'vertices':[]}
+
+    for bound in bounds:
+        item = [
+        {'x': bound.vertices[0].x, 'y': bound.vertices[0].y},
+        {'x': bound.vertices[1].x, 'y': bound.vertices[1].y},
+        {'x': bound.vertices[2].x, 'y': bound.vertices[2].y},
+        {'x': bound.vertices[3].x, 'y': bound.vertices[3].y},
+        ]
+        result['vertices'].append(item)
+
+    return result
